278ed647dc29b4486b2fbd754fe20fadc51820f9
tsoglani <mrtsoglanakos@hotmail.com>
2015-07-18 00:05:46 +0300

    first commit

--- src/mouseapp/MouseApp.java
+++ src/mouseapp/MouseApp.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -36,7 +36,7 @@ public class MouseApp {
 
     public MouseApp() {
         try {
-            bluetoothConnection();
+            internetConnection();
         } catch (Exception ex) {
             ex.printStackTrace();
         }
